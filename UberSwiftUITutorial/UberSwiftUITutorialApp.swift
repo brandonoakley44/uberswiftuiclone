4a276877b6cb568fd59e60cf63b5c7371b6f9c2b
brandonoakley44 <brandonoakley324@gmail.com>
2023-02-21 22:48:05 -0400

    Initial Commit

---
+++ UberSwiftUITutorial/UberSwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberSwiftUITutorialApp.swift
+//  UberSwiftUITutorial
+//
+//  Created by Brandon Oakley on 2023-02-21.
+//
+
+import SwiftUI
+
+@main
+struct UberSwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
